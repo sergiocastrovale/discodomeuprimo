f242180ae04138c473ad99c881126b5240d23eaf
Sérgio Castro <sergio.castro.vale@gmail.com>
2022-11-12 20:20:23 +0000

    Update database (609 artists)

--- build.sh
+++ build.sh
@@ -2,23 +2,27 @@
 if [ -f .env ]; then
   export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
 
+  file="list.txt"
+  file_expanded="list_expanded.txt"
   home=$(pwd)
-  file="list"
-  count=0
+  count=$(find $BASE_PATH -mindepth 1 -maxdepth 1 -type d | wc -l)
   timestamp=`date +"%Y-%m-%d %T"`
 
   if [[ $1 = 'expanded' ]] ; then
-    # WIP
-    echo "Building the list with albums. This will take a while..."
-    tree $BASE_PATH -idf --charset=ISO8859-1 > list_expanded
-    mv list_expanded $home
-    # | sed 's/F:\/mp3\/mainstream\///'
+    echo "*** Building the list with albums. This will take a while..."
+    tree $BASE_PATH -idf --charset=ISO8859-1 >> $file_expanded
+
+    # Remove base path
+    sed -i -z "s%$(head -1 $file_expanded)%%g" $file_expanded
+
+    # Append count and timestamp
+    sed -i "1s/^/${count}|${timestamp}/" $file_expanded
   else
+    echo "*** Building the list..."
     cd $BASE_PATH
 
     for f in */; do
       text+="${f%/}\n"
-      let count++
     done
 
     cd $home
@@ -26,8 +30,9 @@ if [ -f .env ]; then
     echo -n "" > $file
     printf "$count|$timestamp\n" >> $file
     printf "$text" >> $file
-    echo "*** Updated $file ($count artists)"
   fi
+
+  echo "*** Updated list ($count artists)"
 
   if [[ $1 != 'no-deploy' ]] ; then
     git add . && \
